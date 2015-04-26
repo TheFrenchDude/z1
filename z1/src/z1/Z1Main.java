0b767cbce5054a6ff42537606530781e2cdb8af9
dan barhen <frazeit@gmail.com>
2015-04-26 11:49:59 +0100

    back to previous commit

--- z1/src/z1/Z1Main.java
+++ z1/src/z1/Z1Main.java
@@ -2,7 +2,6 @@ package z1;
 
 public class Z1Main {
 	public static void main(String []s){
-		System.out.println("hello world1134599!");
-		this is a bug
+		System.out.println("hello world11345!");
 	}
 }
