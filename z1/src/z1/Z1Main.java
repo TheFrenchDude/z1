8fb76d70bc821072595473d8aea84b84171e9753
dan barhen <frazeit@gmail.com>
2015-04-26 11:47:38 +0100

    adding more bug

--- z1/src/z1/Z1Main.java
+++ z1/src/z1/Z1Main.java
@@ -2,6 +2,7 @@ package z1;
 
 public class Z1Main {
 	public static void main(String []s){
-		System.out.println("hello world11345!");
+		System.out.println("hello world1134599!");
+		this is a bug
 	}
 }
